72b7fd5c929110eea0124b0a61fdf2c7012dfc09
evalphobia <evalphobia@gmail.com>
2016-06-24 14:44:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+logrus_appneta
+====
+
+[![Build Status](https://travis-ci.org/evalphobia/logrus_appneta.svg?branch=master)](https://travis-ci.org/evalphobia/logrus_appneta)
+
+
+# AppNeta TraceView Hook for Logrus <img src="http://i.imgur.com/hTeVwmJ.png" width="40" height="40" alt=":walrus:" class="emoji" title=":walrus:"/>
+
+## Usage
+
+```go
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/evalphobia/logrus_appneta"
+)
+
+func main() {
+	hook := logrus_appneta.NewHook()
+	hook.SetLevels([]logrus.Level{
+		logrus.PanicLevel,
+		logrus.ErrorLevel,
+	})
+
+	logrus.AddHook(hook)
+}
+```
+
+
+## Special fields
+
+Some logrus fields have a special meaning in this hook.
+
+|||
+|:--|:--|
+|`layer`|`layer` is `tv.Layer` type and used to send error. `layer` or `context` is required to send error.|
+|`context`|`context` is `context.Context` type and used to send error. `layer` or `context` is required to send error.|
+|`error`|`error` is `error` type and used for error message |
+|`error_class`|`error_class` is `string` type and used for error class name|
+
+These field can have original prefix.
+
+```go
+	hook := logrus_appneta.NewHook()
+	hook.FieldPrefix = "your_prefix_"
+	logrus.AddHook(hook)
+```
---
+++ appneta.go
@@ -0,0 +1,96 @@
+package logrus_appneta
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/appneta/go-appneta/v1/tv"
+	"golang.org/x/net/context"
+)
+
+const defaultErrorClass = "error"
+
+var defaultLevels = []logrus.Level{
+	logrus.PanicLevel,
+	logrus.FatalLevel,
+	logrus.ErrorLevel,
+}
+
+// AppnetaHook sends error event to AppNeta TraceView
+type AppnetaHook struct {
+	FieldPrefix string
+	ErrorClass  string
+
+	levels []logrus.Level
+}
+
+// NewHook returns initialized *AppnetaHook
+func NewHook() *AppnetaHook {
+	return &AppnetaHook{
+		ErrorClass: defaultErrorClass,
+		levels:     defaultLevels,
+	}
+}
+
+// Fire sends error event
+func (hook *AppnetaHook) Fire(entry *logrus.Entry) error {
+	d := entry.Data
+	ec := hook.getErrorClass(d)
+	msg := hook.getErrorMessage(entry)
+
+	if layer, ok := hook.getLayer(d); ok {
+		layer.Error(ec, msg)
+		return nil
+	}
+
+	if ctx, ok := hook.getContext(d); ok {
+		tv.Error(ctx, ec, msg)
+		return nil
+	}
+
+	return nil
+}
+
+func (hook *AppnetaHook) getErrorClass(d logrus.Fields) string {
+	const key = "error_class"
+
+	if value, ok := d[hook.FieldPrefix+key].(string); ok {
+		return value
+	}
+	return hook.ErrorClass
+}
+
+func (hook *AppnetaHook) getErrorMessage(entry *logrus.Entry) string {
+	const key = "error"
+
+	if err, ok := entry.Data[hook.FieldPrefix+key].(error); ok {
+		return err.Error()
+	}
+	return entry.Message
+}
+
+func (hook *AppnetaHook) getLayer(d logrus.Fields) (tv.Layer, bool) {
+	const key = "layer"
+
+	if value, ok := d[hook.FieldPrefix+key].(tv.Layer); ok {
+		return value, true
+	}
+	return nil, false
+}
+
+func (hook *AppnetaHook) getContext(d logrus.Fields) (context.Context, bool) {
+	const key = "context"
+
+	if value, ok := d[hook.FieldPrefix+key].(context.Context); ok {
+		return value, true
+	}
+	return nil, false
+}
+
+// Levels returns logging level to fire hook
+func (hook *AppnetaHook) Levels() []logrus.Level {
+	return hook.levels
+}
+
+// SetLevels sets logging level to fire hook
+func (hook *AppnetaHook) SetLevels(levels []logrus.Level) {
+	hook.levels = levels
+}
